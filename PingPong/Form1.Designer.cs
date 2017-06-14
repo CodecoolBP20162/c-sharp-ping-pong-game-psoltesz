43832693e4665dd579e55542ca52de8b3011d49a
psoltesz <peter.soltesz92@gmail.com>
2017-06-14 18:29:54 +0200

    basic functions work (mostly), game still needs some bugfixing

--- PingPong/Form1.Designer.cs
+++ PingPong/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PingPong
 {
-    partial class Form1
+    partial class PingPongWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PingPongWindow));
+            this.Paddle = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PlayField = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayField)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Paddle
+            // 
+            this.Paddle.Image = ((System.Drawing.Image)(resources.GetObject("Paddle.Image")));
+            this.Paddle.Location = new System.Drawing.Point(336, 177);
+            this.Paddle.Name = "Paddle";
+            this.Paddle.Size = new System.Drawing.Size(31, 158);
+            this.Paddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Paddle.TabIndex = 0;
+            this.Paddle.TabStop = false;
+            // 
+            // PlayField
+            // 
+            this.PlayField.Location = new System.Drawing.Point(192, 49);
+            this.PlayField.Name = "PlayField";
+            this.PlayField.Size = new System.Drawing.Size(790, 435);
+            this.PlayField.TabIndex = 1;
+            this.PlayField.TabStop = false;
+            this.PlayField.Visible = false;
+            // 
+            // Ball
+            // 
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(773, 218);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(91, 83);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // PingPongWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 567);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Paddle);
+            this.Controls.Add(this.PlayField);
+            this.Name = "PingPongWindow";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.PingPongWindow_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PingPongWindow_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PingPongWindow_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.PingPongWindow_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayField)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Paddle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox PlayField;
+        private System.Windows.Forms.PictureBox Ball;
     }
 }
 
--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -10,11 +10,115 @@ using System.Windows.Forms;
 
 namespace PingPong
 {
-    public partial class Form1 : Form
+    public partial class PingPongWindow : Form
     {
-        public Form1()
+        private bool PaddleMovingUp;
+        private bool PaddleMovingDown;
+        private bool BallMovingUp;
+        private bool BallMovingLeft;
+
+        public PingPongWindow()
         {
             InitializeComponent();
+        }
+
+        private void PingPongWindow_Load(object sender, EventArgs e)
+        {
+            Ball.Location = new Point(600, 200);
+            timer1.Interval = 10;
+            timer1.Tick += new EventHandler(TimerTick);
+            timer1.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            // paddle movement
+            if (PaddleMovingUp && PlayField.Top < (Paddle.Top - 4))
+            {
+                Paddle.Top -= 10;
+            }
+
+            if (PaddleMovingDown && PlayField.Bottom > (Paddle.Bottom + 1))
+            {
+                Paddle.Top += 10;
+            }
+
+            // vertical movement
+            if (BallMovingUp)
+            {
+                Ball.Top -= 2;
+            }
+            if (!BallMovingUp)
+            {
+                Ball.Top += 2;
+            }
+
+            // horizontal movement
+            if (BallMovingLeft)
+            {
+                Ball.Left -= 2;
+            }
+            if (!BallMovingLeft)
+            {
+                Ball.Left += 2;
+            }
+
+            if (Ball.Left == PlayField.Left + 4)
+            {
+                timer1.Stop();
+                MessageBox.Show("You lost");
+            }
+            if (Ball.Right >= PlayField.Right - 3)
+            {
+                BallMovingLeft = true;
+            }
+            //send it to bottom
+            if (Ball.Top == PlayField.Top + 4)
+            {
+                BallMovingUp = false;
+            }
+            // send it to top
+            if (Ball.Bottom >= PlayField.Bottom - 3)
+            {
+                BallMovingUp = true;
+            }
+            // paddle contact
+            if (Ball.Left == Paddle.Right)
+            {
+                BallMovingLeft = true;
+            }
+        }
+
+        private void PingPongWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                PaddleMovingUp = false;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                PaddleMovingDown = false;
+            }
+        }
+
+        private void PingPongWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                PaddleMovingUp = true;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                PaddleMovingDown = true;
+            }
+        }
+
+        private void PingPongWindow_Paint(object sender, PaintEventArgs e)
+        {
+            Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 5);
+            e.Graphics.DrawRectangle(blackPen, PlayField.Location.X, PlayField.Location.Y, PlayField.Size.Width, PlayField.Size.Height);
         }
     }
 }
